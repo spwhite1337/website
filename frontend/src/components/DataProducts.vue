c086cafc2c54e54a8ea633279e8c8a4b73ce098c
Scott White <spwhite1337@gmail.com>
2020-07-26 15:44:48 -0400

    Add bootstrap package

--- frontend/src/components/DataProducts.vue
+++ frontend/src/components/DataProducts.vue
@@ -3,6 +3,13 @@
     <p>Data Products</p>
     <a href="./">Return Home</a>
 
+    <!--  Product Selector  -->
+    <b-dropdown id="dropdown-left" text="Left align" variant="primary" class="m-2">
+      <b-dropdown-item href="#">Action</b-dropdown-item>
+      <b-dropdown-item href="#">Another action</b-dropdown-item>
+      <b-dropdown-item href="#">Something else here</b-dropdown-item>
+    </b-dropdown>
+
     <!--   Sports Bettors   -->
     <div>
       <hr>
