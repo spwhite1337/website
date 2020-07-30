f3ae9268f60b1564c39c1ead71a3f048ab420eb0
Scott White <spwhite1337@gmail.com>
2020-07-29 21:42:42 -0400

    one button

--- frontend/src/components/TestBackend.vue
+++ frontend/src/components/TestBackend.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="hello">
-    <button @click="testBackend1">Call backend</button>
-    <button @click="testBackend2">Call backend</button>
+    <button @click="testBackend">Call backend</button>
     <h2>{{ test.test }}</h2>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-const path1 = '/api/test'
-const path2 = 'http://backend:8080/api/test'
+const path = '/api/test'
 export default {
   name: 'TestBackend',
   data () {
@@ -18,24 +16,15 @@ export default {
     }
   },
   methods: {
-    testBackend1 () {
-      axios.get(path1).then(response => {
+    testBackend () {
+      axios.get(path).then(response => {
         this.test = response.data.test
         })
         .catch(error => {
           console.log(error)
           })
           return {}
-        },
-    testBackend2 () {
-      axios.get(path2).then(response => {
-        this.test = response.data.test
-        })
-        .catch(error => {
-          console.log(error)
-          })
-          return {}
-        },
-      }
+        }
+     }
 }
 </script>
