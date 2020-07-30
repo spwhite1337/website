80fa8a17df7a9ba253a4948e39e236a9b5160d96
Scott White <spwhite1337@gmail.com>
2020-07-30 11:48:32 -0400

    PAges routed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,10 @@
     <a href="./DataProducts">Data Products</a>
     <a href="./Blog">Blog</a>
     <img src="./assets/logo.png">
+    <p>
+      You are in App.vue
+    </p>
+    <router-view/>
   </div>
 </template>
 
