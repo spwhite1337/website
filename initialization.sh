5b80c0bb51fa3577034da490d3dd8409d9f68aa1
Scott White <spwhite1337@gmail.com>
2020-08-02 01:15:04 -0400

    initalization sh

---
+++ initialization.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+# Install Docker
+sudo apt-get update
+sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt -y install docker-ce
+# Docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Set up project
+# Go to instance root (on EC2; cd ../../ when default connection places you in home/ubuntu)
+sudo git clone https://github.com/spwhite1337/website.git
+cd website/backend
+sudo apt -y install awscli
+aws configure
+    Add AWS Keys
+sudo python3 download.py
+cd ../
