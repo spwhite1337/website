8f16a3c6e55d3904dfe87eed540259b0bf022e47
Scott White <spwhite1337@gmail.com>
2020-08-02 02:15:54 -0400

    Update comment

--- initialization.sh
+++ initialization.sh
@@ -6,7 +6,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 sudo apt -y install docker-ce
-# Docker-compose
+# Install Docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
