6d1a7c473bcdef0f812248320a2c8569ae82a10e
Scott White <spwhite1337@gmail.com>
2020-08-03 11:53:15 -0400

    Add not found router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,14 +3,16 @@ import Router from 'vue-router'
 import DataProducts from '@/components/DataProducts.vue'
 import Home from '@/components/Home.vue'
 import Blog from '@/components/Blog.vue'
+import NotFound from '@/components/NotFound.vue'
 
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
     routes: [
-        { path: '/', name: 'home', component: Home},
-        { path: '/DataProducts', name: 'DataProducts', component: DataProducts},
-        { path: '/Blog', name: 'Blog', component: Blog}
+        { path: '/', name: 'home', component: Home },
+        { path: '/DataProducts', name: 'DataProducts', component: DataProducts },
+        { path: '/Blog', name: 'Blog', component: Blog },
+        { path: '*', name: 'NotFound', component: NotFound }
     ]
 })
