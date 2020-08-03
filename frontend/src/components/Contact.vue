a64076ceaa6f3b401bdf2ecd567d23e67acab072
Scott White <spwhite1337@gmail.com>
2020-08-03 19:24:05 -0400

    Better contact page

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -2,10 +2,8 @@
     <ul>
         <li>Email (Preferred): spwhite1337@gmail.com</li>
         <li><a href="https://www.youtube.com/user/spwhite1337" target="_blank">Youtube Channel for SPWhite1337</a></li>
-        <li>Give me money on <a href="https://venmo.com/hubba" target="_blank">Venmo</a> or give it to poor people</li>
-        <li>If I post on social media I mostly do
-            <a href="https://twitter.com/ScottDollaBillz" target="_blank">Twitter</a>
-        </li>
+        <li>Give me money on <a href="https://venmo.com/hubba" target="_blank">Venmo</a> if you like</li>
+        <li><a href="https://twitter.com/ScottDollaBillz" target="_blank">Twitter</a> is my platform of choice</li>
         <li><a href="https://www.linkedin.com/in/scott-p-white/" target="_blank">LinkedIn</a></li>
         <li><a href="https://github.com/spwhite1337" target="_blank">GitHub</a></li>
         <li><a href="https://www.instagram.com/scottdollabills/" target="_blank">
