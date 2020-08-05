0141111401f9fe7a2e81076bd6d9279b37eded9d
Scott White <spwhite1337@gmail.com>
2020-08-05 10:33:58 -0400

    Add plotly dash apps to flask

--- backend/apis/dash/card_classifier.py
+++ backend/apis/dash/card_classifier.py
@@ -2,13 +2,15 @@ import dash
 import dash_html_components as html
 
 
-def add_sb_dash(server, routes_pathname_prefix: str = '/api/dash/sportsbettors/'):
+def add_cc_dash(server, routes_pathname_prefix: str = '/api/dash/cardclassifier/'):
+    """
+    Add a
+    """
     dashapp = dash.Dash(
         __name__,
         routes_pathname_prefix=routes_pathname_prefix,
         server=server
     )
-    dashapp.layout = html.H1('Hi From Dash (sports bettors)')
+    dashapp.layout = html.H1('Hi From Dash (card classifier)')
 
     return dashapp.server
-
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,9 +1,12 @@
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-
 from run import app
-from apis.dash.sports_bettors import dashapp as sb_app
+from apis.dash.sports_bettors import add_sb_dash
+from apis.dash.card_classifier import add_cc_dash
 
-app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {'/api/dash/': sb_app.server})
+# Add Sports-Bettors Dash Board to Flask Server
+app = add_sb_dash(app, routes_pathname_prefix='/api/dash/sportsbettors/')
+
+# Add Card Classifier Dash Board to Flask Server (as an example of multiple dash boards)
+app = add_cc_dash(app, routes_pathname_prefix='/api/dash/cardclassifier/')
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
