f7bb699f40f2d7a17c987e4fbc6371106732247b
Scott White <spwhite1337@gmail.com>
2020-08-05 11:18:54 -0400

    No curly

--- frontend/src/components/data_products/SportsBettors.vue
+++ frontend/src/components/data_products/SportsBettors.vue
@@ -18,7 +18,7 @@
     {{ dashboard }}
     <br>
     <iframe src="https://scottpwhite.com/api/dash/sportsbettors" frameborder="0"></iframe>
-    <iframe :src="{ dashboard }" frameborder="0"></iframe>
+    <iframe :src="dashboard" frameborder="0"></iframe>
     
   </div>
 </template>
