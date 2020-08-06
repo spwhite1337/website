77c4955a45d481acb5a9cdd96e128b7d67f05f80
Scott White <spwhite1337@gmail.com>
2020-08-05 22:58:13 -0400

    Add transitions

--- frontend/src/components/Blog.vue
+++ frontend/src/components/Blog.vue
@@ -19,7 +19,10 @@
             <router-link :to="{ name: post.name }">{{ post.title }}</router-link>
           </b-dropdown-item>
       </b-dropdown>
-    <router-view></router-view>
+
+      <transition name="slide-fade">
+          <router-view></router-view>
+      </transition>
   </div>
 </template>
 
@@ -35,3 +38,19 @@ data () {
     }
 }}
 </script>
+
+<style>
+  /* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
