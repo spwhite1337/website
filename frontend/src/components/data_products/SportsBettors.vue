b2e7d26a1a7ef7f23a37bbf55cb718cd0bf8b723
Scott White <spwhite1337@gmail.com>
2020-08-10 00:06:18 -0400

    Unsized iframe

--- frontend/src/components/data_products/SportsBettors.vue
+++ frontend/src/components/data_products/SportsBettors.vue
@@ -116,22 +116,4 @@ export default {
     }
   }
 }
-
-function resizeIFrameToFitContent( iFrame ) {
-
-    iFrame.width  = iFrame.contentWindow.document.body.scrollWidth;
-    iFrame.height = iFrame.contentWindow.document.body.scrollHeight;
-}
-
-window.addEventListener('DOMContentLoaded', function(e) {
-
-    var iFrame = document.getElementById( 'iFrame1' );
-    resizeIFrameToFitContent( iFrame );
-
-    // or, to resize all iframes:
-    var iframes = document.querySelectorAll("iframe");
-    for( var i = 0; i < iframes.length; i++) {
-        resizeIFrameToFitContent( iframes[i] );
-    }
-} );
 </script>
