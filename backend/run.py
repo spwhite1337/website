3424e3d9b67a4e26274e478959ff4e6c2f2699a1
Scott White <spwhite1337@gmail.com>
2020-08-10 08:38:14 -0400

    Better backend structure

--- backend/run.py
+++ backend/run.py
@@ -18,32 +18,39 @@ app = add_sb_dash(app, routes_pathname_prefix='/api/dash/sportsbettors/')
 
 @app.route('/api/sportsbettors', methods=['GET', 'POST'])
 def sports_bettors():
-    response = jsonify({'sb_output': 'blank'})
+    # response = jsonify({'sb_output': 'blank'})
     if request.method == 'GET':
-        if 'league' in request.args.keys():
-            response = sb_api(request.args)
-            return jsonify({'sb_output': response})
-    return response
+        # if 'league' in request.args.keys():
+        #     response = sb_api(request.args)
+        #     return jsonify({'sb_output': response})
+        output = sb_api(inputs=request.args) if 'league' in request.keys() else 'blank'
+    else:
+        output = 'blank'
+    return jsonify({'sb_output': output})
 
 
 @app.route('/api/cardclassifier', methods=['GET', 'POST'])
 def card_classifier():
-    response = jsonify({'card_color': 'blank'})
     if request.method == 'GET':
-        if 'default_card' in request.args.keys():
-            output = cc_api(default_card=request.args['default_card'])
-            return jsonify({'card_color': output})
-    return response
+        # if 'default_card' in request.args.keys():
+            # output = cc_api(default_card=request.args['default_card'])
+            # return jsonify({'card_color': output})
+        output = cc_api(default_card=request.args['default_card']) if 'default_card' in request.args.keys() else 'blank'
+    else:
+        output = 'blank'
+    return jsonify({'card_color': output})
 
 
 @app.route('/api/presidentsspeeches', methods=['GET', 'POST'])
 def presidents_speeches():
-    response = {'president': 'blank'}
     if request.method == 'GET':
-        if 'query' in request.args.keys():
-            output = ps_api(request.args['query'])
-            return jsonify({'president': output})
-    return response
+        # if 'query' in request.args.keys():
+        #     output = ps_api(request.args['query'])
+        #     return jsonify({'president': output})
+        output = ps_api(query=request.args['query']) if 'query' in request.args.keys() else 'blank'
+    else:
+        output = 'blank'
+    return jsonify({'president': output})
 
 
 @app.route('/', defaults={'path': ''})
