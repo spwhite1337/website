acc35384439d5a76ea500a9c670fe4de71f0f675
Scott White <spwhite1337@gmail.com>
2020-08-09 23:23:25 -0400

    100 px div

--- frontend/src/components/data_products/SportsBettors.vue
+++ frontend/src/components/data_products/SportsBettors.vue
@@ -15,7 +15,7 @@
     <button @click="sportsBettors">Bet on Sports</button>
     <p>Output from Sports Bettors: {{ sb_output }}</p>
     <br>
-    <div class="container" height="100%" width="100%">
+    <div class="container" height="100px" width="100%">
       <iframe :src="dashboard" frameborder="0"></iframe>
     </div>
 
