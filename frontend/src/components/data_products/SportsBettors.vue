a0c224247f423cbb7d6f4b0dccdf9aa09874c3ba
Scott White <spwhite1337@gmail.com>
2020-08-15 18:08:58 -0400

    Line breaks in sports bettros

--- frontend/src/components/data_products/SportsBettors.vue
+++ frontend/src/components/data_products/SportsBettors.vue
@@ -7,12 +7,12 @@
       The great state of Indiana has introduced me to the world of online sports betting and it is a real treat.
       I primarily use FanDuel to take advantage of combination bets including (i) which team will win, (ii) what
       the spread will be, and (iii) the over / under for total points in the game.
-      <br>
+      <br><br>
       The dashboard below presents results from a bayesian regression on three responses: (i) the probability of the
       selected team winning against the select opponent, (ii) the probability the spread or margin of victory will be
       at least as high as presented, and (iii) the total number of points in the game. The results are specific to a
       model that control for different sets of inputs.
-      <br>
+      <br><br>
       See the code and diagnostics of the various models on the repo:
       <a href="https://github.com/spwhite1337/sports-bettors" target="_blank">Repo</a>
     </p>
