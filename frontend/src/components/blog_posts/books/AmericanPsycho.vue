f873810700d4144e51ba689e4ed7b075c98ec3ca
Scott White <spwhite1337@gmail.com>
2020-08-16 15:08:24 -0400

    Revise american Psycho

--- frontend/src/components/blog_posts/books/AmericanPsycho.vue
+++ frontend/src/components/blog_posts/books/AmericanPsycho.vue
@@ -8,7 +8,7 @@
        amoral, and (most importantly) soulless. On first glance, the ending seems to inspire eyerolls with the reality
        of the story brought into question in the final minutes. But I will argue that this is perfectly coherent and
        consistent with the character - which is actually a non-character in a non-story. If there is a point to the
-       story, its that a story about the personification of capitialism is pointless or meaningless. A notion that
+       story, its that a story about the personification of Capitalism is pointless and meaningless. A notion that
        treats relationships, jobs, and marriage as significantly as it treats murder, rape, and cannibalism. Namely,
        not significantly.
        <br>
@@ -16,8 +16,10 @@
        <br>
        <b>Summary:</b> The story focuses on one character, Patrick Bateman, and his existence as a high end wall street
        account manager. While impressive on paper, his job is deeply blended with his social life with little-to-no
-       emphasis on his actual profession. He explains his job to flirt with women, but he never actually does what
+       emphasis on his actual profession. He explains his job to flirt with women, but he never actually says what
        his description is. He has business meetings, but they never talk about business.
+       <br>
+       <img :src="getImgUrl('bateman.jpg')" alt="Patrick Bateman" width="25%">
        <br><br>
        Instead, his life, relationships, and work revolve around his position in a social hierarchy defined by external
       appearance, job title, and wealth. He constantly obsesses with clothing, exercise, beauty products, musical tastes
@@ -25,12 +27,14 @@
       <a href="https://www.youtube.com/watch?v=cISYzA36-ZY" target = "_blank"> Business Cards</a>.
        <br><br>
        In a violent, graphic, and grotesque display of writing Patrick Bateman embarks in escalating acts of depravity.
-       One of many examples is presented here and was selected because it made me laugh out loud in absurdity. These scenes
-      are sprinkled throughout the novel with a clear progression of intensity. Bateman starts by accosting homeless
-      people and dogs, advances to prostitutes, and finished with a murder of a colleague.
+       The extent to which the acts are grotesque is not really the point, but rather the fact that Bateman can get up
+       the next day unphased.
+      <br><br>
+       One of many examples is presented here and was selected because it made me laugh out loud in absurdity.
        <br><br>
+       <em>
        I lean in above her and shout, over her
-       screans, "Try to scream, scream, keep screaming..." I've opened all the windows and the door to my
+       screams, "Try to scream, scream, keep screaming..." I've opened all the windows and the door to my
        terrace and when I stand over her, the mouth opens and not even screams come out anymore, just
        horrible, guttural, animal-like noises, sometimes interrupted by retching sounds. "Scream, honey," I
        urge, "keep screaming," I lean down, even closer, brushing her hair back. "No one cares. No one will
@@ -58,36 +62,41 @@
             <br><br>
        Later, the next night in fact, three of us, Craig McDermott, Courtney and myself, are in a cab heading
        toward Nell's and talking about Evian water......
-      <br><br>
-      Parallels with works like Dorian Gray are clear, though,
-      American Psycho goes into excruciating detail wwhile Oscar Wilde leaves it to the imagination.
+       </em>
       <br><br>
       The story zig-zags between inner monologues about minutiae, grotesque violent acts, elaborate social gossip,
       and insider business deals. While the violence sharply increases, the other aspects of his life are largely
       meander aimlessly.
-      He starts with a girlfriend, advances to engagement with zero romance, break it off
-      over dinner, get back together, etc. There are big accounts at work that come and go, we aren't given justification
-      for their significance, nor are we even told what professional role Bateman plays. His violence culminates with an
-      axe murder of his competitor and friend, Paul Allen.
+      <br>
+      <img :src="getImgUrl('chainsaw.jpg')" alt="Violence" width="25%">
       <br><br>
-      The ending can be very confusing if these story lines
-      are emphasized or used to contextualize it. To refresh your memeory - In
-      the final chapters after the murder, Patrick Bateman makes a call to his lawyer to confess his crimes only for it
-      to be received as a joke and that Paul Allen is alive and on vacation. This "twist" is often interpreted as a
-      consequence of Bateman's psychosis, his life being a dream, him being out of touch with reality, etc.
+      He starts with a girlfriend, advances to engagement with zero romance, breaks it off
+      over dinner, they get back together, etc. There are big accounts at work that come and go, we aren't given
+      an elaboration of their significance, nor are we even told what professional role Bateman plays.
+      Meanwhile, sis violence systematically escalates and culminates with an axe murder of his competitor and friend,
+      Paul Allen. I repeat, the idea is to blend these extreme contrasts in Bateman's experiences in a character
+     that goes about them unfeeling. Bateman is nothing, just a mechanism or system that requires constant validation
+     to persist. He is an embodiment of Capitalism and vacuous, greed-driven lifestyle.
       <br><br>
+     <b>Finale:</b> In the final chapters after the murder of Paul Allen, Patrick Bateman makes a call to his lawyer to
+      confess his crimes only for it to be received as a joke and to learn that Paul Allen is alive and on vacation.
+      This "twist" is often interpreted as a consequence of Bateman's psychosis, his life being a dream, him being out
+      of touch with reality, etc.
+      <br><br>
+      <img :src="getImgUrl('paulallen.jpg')" alt="Paul Allen" width="25%">
+      <br>
       I just don't agree and encourage you to think otherwise. I see Bateman's life as real in every normal sense,
       his actions have consequences, he affects the
       environment around him, but none of it matters. He is a meaningless entity. Nothing "happens" to <em>him</em>.
       The story is neither a tragedy nor a comedy, it is nothing. The ending
       is both real and not-real. But it definitely didn't matter. There is no significance to Patrick's
       violence because Patrick is nothing of significance. He can go on living his life but it isn't
-      actually a life at all. The book is entirely meaningless and beautifully exemplified by the extremely
+      actually a life at all. The book is entirely meaningless as expertly exemplified by the extremely
       grotesque chapters that comprise the ever increasing descent into nothingness. The story doesn't
       climax it repeats, and it is just as shallow as it ever was.
       <br><br>
-      Patrick Bateman is a representation of wall-stree capitalism. It doesn't provide concrete value,
-      it is amoral, and it does not lend itself to interesting interpretation. Audiences err when they see American
+      As a representative of Wall-Street Capitalism, Patrick Bateman doesn't have intrinsic significance,
+      he is amoral, and he does not lend itself to interesting interpretation. Audiences err when they see American
       Psycho as a <em>character piece</em>, or an adventurous thriller. It is a story about nothing. The violence
       doesn't matter any more than business cards matter.
       <br><br>
@@ -96,9 +105,9 @@
         <br><br>
        <b>Drug Use:</b> The drug use in American Psycho is interesting for its stark contrast with the
       romanticization of drugs from the 60s and 70s. Rather than portraying drugs as a pathway to a kind of
-      enlightenment, Patrick Batemans seems that as a means to an end. He needs them to get through work,
-      stave off a hangover (from alcohol which was necessary to be in the NY scene), have a good workout, enjoy time with his
-      significant other. I highly doubt the author intended it this way, but it highlights the vacuousness of
+      enlightenment, Patrick Batemans sees them as a means to an end. He needs them to get through work,
+      stave off a hangover (from alcohol which was necessary to be in the NY scene), have a good workout, enjoy time
+      with his significant other. I highly doubt the author intended it this way, but it highlights the vacuousness of
       material or substantive pleasures in a hilarious manner. Drugs specifically designed to control your emotions
       are only used to maintain a version of yourself to others (Bateman's whole being).
       <br><br>
@@ -106,7 +115,8 @@
       stem from. Even in the face of extreme, life-defining acts of violence - Bateman can only metaphorically
       speak of drug highs to express himself.
       <br><br>
-      [After tormenting a homeless man] Then I turn to the barking dog and when I get up, stomp on its
+      <em>
+       [After tormenting a homeless man] Then I turn to the barking dog and when I get up, stomp on its
        front legs while it's crouched down ready to jump at me, its fangs bared, immediately shattering
        the bones in both its legs, and it falls on its side squeaking in pain, front paws sticking up in the
        air at an obscene, satisfying angle. I can't help but start laughing and I linger at the scene,
@@ -123,28 +133,32 @@
        Club walk in after me and sit in the booth across from mine, both smiling flirtatiously. I play it cool
        and ignore them. An old, crazy woman, wrinkled, chain-smoking, sits near us, nodding at no one.
        <br><br>
-       <b>Women/Misogyny:</b> Women are objectified and used as sexual receptors and status symbols. The
-      progression of the relationship to engagement/marriage is necessary only so far as it helps them
+      </em>
+       <b>Women/Misogyny:</b> Women are objectified and used as sexual receptors and / or a symbol of status for
+      Bateman. The progression of the relationship to engagement/marriage is necessary only so far as it helps them
       with respect to their social circle. Bateman frequently refers to women as "hardbodies" as he would
-      never look at them any deeper than that (he isn't any deeper himself). He treats prostitutes as
+      never look at them any deeper than that (he can't since he isn't any deeper himself). He treats prostitutes as
       sex robots that he only fucks to exemplify his capabilities. The most significant sexual attraction
       he has is to himself, as he stares at himself and flexes in the mirror while fucking a prostitute from behind.
       <br><br>
-      Like drugs, women are not romantized and used as a means to an end. Unlike drugs, the end isn't a please but a
+      <img :src="getImgUrl('evelyn.jpg')" alt="Evelyn" width="25%">
+      <br>
+      Like drugs, women are not romanticized and used as a means to an end. Unlike drugs, the end isn't a pleasure but a
       validation. Bateman must constantly demonstrate his worth because he has no "being" to ground himself in. He has
-      no emotions, and no opinion of himself. His sex is not romantic or pleasure, it is best described as a
-      valueless insertion of genitalia.
+      no emotions, and no opinion of himself. His sex is not romantic or pleasurable, it is best described as a
+      valueless insertion of genitalia that reinforces himself to others.
       <br><br>
       <b>Art and Advertisements:</b> Novels and movies are types of art, American Psycho is both a novel and movie,
-      making it a work of art. Logic. However, the portrayal of art in American Psycho is the biggest, boldest
-      arrow pointing to a shallow, meaningless interpretation. Despite always listening to music and renting video
-      tapes, Patrick Bateman never comments on them with any depth. He doesn't say how the music makes *him* feel,
+      making it a work of art (Logic). However, the portrayal of art in American Psycho is the biggest, boldest
+      arrow pointing to a shallow, meaningless philosophy expressed by it. Despite always listening to music and renting
+      video tapes, Patrick Bateman never comments on them with any depth. He doesn't say how the music makes *him* feel,
       but rather how the music *is*. He gives generic, uncontroversial analysis of
       <a href="https://www.youtube.com/watch?v=Ruw9fsh3PNY" target="_blank">Huey Lewis and the News</a>,
       Phil Collins, and more but never relates the music to his life or his emotions (like, you know, real people).
       <br><br>
       The closest we get to a penetrating piece of art for Bateman follows:
       <br><br>
+      <em>
        But when I sit down something strange on the stage catched my eye. Bono has
        now moved across the stage, following me to my seat, and he's staring into my eyes, kneeling at the
        edge of the stage, wearing black jeans (maybe Gitano), sandals, a leather vest with no shirt
@@ -163,6 +177,7 @@
        body alive and burning on fire, and from nowhere a flash of white and blinding light envelopes me and I
        hear it, can actually feel, can even make out the letters of the message hovering above Bono's head in
        orange wavy letters: "I... am... the... devil... and I am... just... like... you...
+      </em>
       <br><br>
       You, as a viewer, should interpret American Psycho in the way Patrick Bateman interprets 80s pop music. "Christian
       Bale looks very good while he is killing a cat", "The camera angle during the slaughter of prostitutes is very
@@ -170,10 +185,11 @@
       period piece. It is nothing, just ink on paper or lights on a screen.
       <br><br>
       <b>Still Don't Believe Me?:</b> For a final straw, to prevent you from looking into this work
-      more than you should. To stop you from thinking, "Man, is American Psycho a metaphor for insanity or mental illness?".
-      To make sure you realize, all of Bateman's live is insignificant, meaningless, and unnecessary to
+      more than you should. To stop you from thinking, "Man, is American Psycho a metaphor for insanity or
+      mental illness?". To make sure you realize, all of Bateman's live is insignificant, meaningless, and unnecessary to
       interpret. I will lend you Bateman's explicit admission and agreement with me.
       <br><br>
+      <em>
        "And I just can't..." She stops, frustrated. "I can't pretend these feelings don't
        exist, can I?"
            "Shhh..."
@@ -181,32 +197,38 @@
        an entity, something illusory, and though I can hide my cold gaze and you can shake my hand and feel
        flesh gripping yours and maybe you can even sense our lifestyles are probably comparable, I simply am
        not there. It is hard for me to make sense of any given level. Myself is fabricated, an aberration.
-       I am a noncontingent human being. My personality is sketchy and unformed, my heartlessness goes
+       I am a non-contingent human being. My personality is sketchy and unformed, my heartlessness goes
        deep and is persistent. My conscience, my pity, my hopes disappeared a long time ago (probably at
        Harvard) if they ever did exist. There are no ore barriers to cross. All I have in common with the
-       uncontrollable and the insane, the vicious and th eevil, all the mayhem I have caused and my utter
-       indifference toward it, I have now surpassed. I stil, though, hold on to the one
+       uncontrollable and the insane, the vicious and the evil, all the mayhem I have caused and my utter
+       indifference toward it, I have now surpassed. I still, though, hold on to the one
        single bleak truth: no one is safe, nothing is redeemed. Yet I am blameless. Each model of human
-       behavior must be assumed to have some validity. Is evel something you are? Or is it something you
+       behavior must be assumed to have some validity. Is evil something you are? Or is it something you
        do? My pain is constant and sharp and I do not hope for a better world for anyone. In fact I want my
        pain to be inflicted on others. I want no one to escape. But even after admitting this - and I have,
        countless times, in just about every act I've committed - and coming face-to-face with these truths,
        there is no catharsis. I gain no deeper knowledge about myself, no new understanding can be
        extracted from my telling. There has been no reason for me to tell you any of this. This confession
-       has meant nothing. <br>
-          I'm asking Jean, "How many people in this world are like me?" <br>
+       has meant nothing. <br><br>
+          I'm asking Jean, "How many people in this world are like me?" <br><br>
 
-         She pauses, carefullly answers, "I don't ... think anyone?" She's guessing. <br>
+         She pauses, carefully answers, "I don't ... think anyone?" She's guessing. <br><br>
 
          "Let me rephrase the ques- Wait, how does my hair look?" I ask, interrupting myself. <br>
 
          "Uh, fine." <br>
+      </em>
       <br><br>
  </div>
 </template>
 
 <script>
 export default {
- name: 'AmericanPsycho'
-}
+ name: 'AmericanPsycho',
+    methods: {
+        getImgUrl (pic) {
+                return require('@/assets/blog_posts/books/AmericanPsycho/' + pic)
+            }
+        },
+    }
 </script>
