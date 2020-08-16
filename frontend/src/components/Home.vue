d826bee5dd539d239c2220f7854eb5b395f4eab1
Scott White <spwhite1337@gmail.com>
2020-08-15 23:34:51 -0400

    Add a Home page

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>
-    <p>Scott P. White's page</p>
+  <div class="container">
+    <img src="getImgUrl('headshot.png')" alt="Headshot" width="25%">
+    <p>
+      I'm Scott White and welcome to my channel. I hail from the great state of Iowa but have since done a
+      Midwest tour to Minneapolis, Chicago, and Indianapolis.
+      <br><br>
+      I am an academy-certified Chemical Engineer with a BS from the University of Iowa and a PhD from The
+      University of Minnesota but I am a private-sector-certified Data Scientist working for a consulting firm in 
+      Chicago, (<a href="https://www.strong.io/" target="_blank">Strong Analytics</a>, and currently at
+      <a href="http://aes.com/" target="_blank">AES</a>, a energy company with a heavy renewable portfolio.
+      <br><br>
+      Far more importantly, I am a fiance to Brittany Forkus and a dog-keeper to Indy the Yorkie Terror.
+      Ideologically I'm a tortured combination of Catholic and Liberal with the worst parts of each one
+      contradictorily mixed together. Though the "Catholic" part should be mixed with "American" and integrated
+      beyond an ideology but I'm too liberal to make any coherent sense of it. I also subscribe to the ideology that
+      doesn't like to put labels on anything but I think that just means I'm a Millennial.
+      <img src="getImgUrl('indy.JPG')" alt="Indy" width="25%">
+      <br><br>
+      Explore the Data Products section to see some examples of my Data Analytics skill, the Sports Bettors app is my
+      favorite.
+      <br><br>
+      The Blog is all over the place, but mostly serves as a repository of Book reviews for pretentious stuff I read.
+    </p>
   </div>
 </template>
+
+<script>
+export default {
+    name: 'Home',
+    methods: {
+        getImgUrl (pic) {
+                return require('@/assets/Home/' + pic)
+            }
+        },
+    }
+</script>
