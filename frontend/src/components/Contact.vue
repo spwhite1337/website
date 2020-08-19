43692a76c28e25220f834c311cc8361df502bd6b
Scott White <spwhite1337@gmail.com>
2020-08-19 17:24:10 -0400

    Download resume button

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -1,7 +1,7 @@
 <template>
     <ul>
         <li>Email (Preferred): spwhite1337@gmail.com</li>
-        <li><a @click="showResume"><b>Resume (not totally up-to-date)</b></a></li>
+        <li><button><a @click="showResume">Download Resume (Updated Summer 2020)</a></button></li>
         <li><a href="https://www.youtube.com/user/spwhite1337" target="_blank">Youtube Channel for SPWhite1337</a></li>
         <li>Give me money on <a href="https://venmo.com/hubba" target="_blank">Venmo</a> if you like</li>
         <li><a href="https://twitter.com/ScottDollaBillz" target="_blank">Twitter</a> is my platform of choice</li>
@@ -21,10 +21,6 @@ import Resume from '@/assets/Contact/ScottWhite_Resume.pdf'
 
 export default {
     name: 'Contact',
-    methods: {
-        showResume () {
-            window.open(Resume)
-            }
-        },
-    }
+    methods: { showResume () { window.open(Resume) } },
+}
 </script>
