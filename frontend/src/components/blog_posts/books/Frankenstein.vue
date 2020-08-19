32a449f9a844cf89b3b1f6b094955ddd7a1213ba
Scott White <spwhite1337@gmail.com>
2020-08-19 17:48:44 -0400

    Add images to frankenstein

--- frontend/src/components/blog_posts/books/Frankenstein.vue
+++ frontend/src/components/blog_posts/books/Frankenstein.vue
@@ -4,26 +4,29 @@
     <h2>Frankenstein, by Mary Shelley</h2>
     <hr>
      Often considered the first authentic work of science fiction, Frankenstein is a classic horror novel by Mary
-    Shelley, published in 1823. It is a super dark book that toys with fundamental questions about why beings exist in the
-   first place, what it means to be alive, and who has the responsibility to create and maintain life. Its presented in an
-   accessible manner that enables the reader to progress through the plot without triggering an existential crisis of
+    Shelley published in 1823. It is a super dark book that toys with existential questions about the motivation for
+    existence, what it means to be alive, and who has the responsibility to create and maintain life. Its presented in an
+    accessible manner that enables the reader to progress through the plot without triggering an existential crisis of
     their own.
      <br><br>
      I will give a brief summary of the book then address three questions the novel raises. It is important to note
-    that the novel arises out of a context that seriously blurred the distinctions between science, alchemy, and the
-    metaphysics of life. Europe was slowly transitioning to a continent with a plurality of knowledge or truth sources
-    including the Church, the Public, and Secular Institutions of Science and Philosophy.
+    that the novel arises in a context that seriously blurred the distinctions between science, alchemy, and the
+    metaphysics of life relative to 21st century America. Europe was slowly transitioning to a continent with a
+    plurality of knowledge or truth sources including the Church, the Public, and Secular Institutions of Science and
+    Philosophy.
      <br><br>
+     <img :src="getImgUrl('frankenstein.jpg')" alt="Frankenstein" width="25%">
+     <br>
      <hr>
      <b>Part 0, Introduction on the Ship:</b> The book follows a segmented pattern with different narrators in each
  section. The first tracks a shipping expedition to the North Pole that finds a strange creature (unnamed monster)
  sledding across the icebergs followed by an emaciated old man (Dr. Frankenstein). They bring the gentleman on board
- where he tells his life story as a moral lesson against reckless adventures into nature.
+ where he tells his life story as a moral lesson against reckless meddling with nature.
      <br><br>
      <b>Section 1, Frankenstein's Story:</b> The main storyline starts with Dr. Frankenstein's upper-class upbringing
  and early interest in philosophy and alchemy. After his mother tragically dies of illness, Frankenstein becomes
  engrossed in the study of chemistry (alchemy) and the origins life. He eventually makes a creature he hoped would
- be a beautiful specimen of life to epitomize his noble aspirations:
+ be a beautiful specimen to epitomize his noble aspirations:
      <br><br>
      <em>
        "So much has been done, exclaimed the soul of Frankenstein — more, far more, will I achieve; treading in the
@@ -31,7 +34,7 @@
       mysteries of creation."
      </em>
      <br><br>
-     However, the project takes a turn for the worse when the creature arises as a monstrosity that escapes.
+     However, the project takes a turn for the worse when the creature arises as a monstrosity and escapes.
  Time passes and William, Frankenstein's brother, is murdered. The town expects Justine, William's nanny, and
  puts her on trial. However, Frankenstein witnessed the monster near the crime scene but is unable to protect
  his friend due to the ludicrousness of his testimony - resulting in the hanging of Justine.
@@ -54,12 +57,16 @@
  to fear of evil plaguing the Earth, Frankenstein aborts the project much to the chagrin of the monster. Confronting
  Frankenstein, the monster claims revenge on Frankenstein's wedding night. Frankenstein then finds that his friend,
  Henry, is murdered (inferred to be done by the monster) and is thrown in prison as a suspect.
+    <br>
+    <img :src="getImgUrl('drfrankenstein.jpg')" alt="Dr. Frankenstein" width="25%">
      <br><br>
      Frankenstein is eventually released due to insufficient evidence and returns to marry his lifelong lover,
  Elizabeth. Terrified of what might happen with the monster, he arms himself and guards against attacks on his
  wife. The monster sneaks past him (due to his superior intellect and strength) and kills his newlywed wife.
  Frankenstein chases the monster all the way to the North Pole where the story rejoins the shipping expedition.
      <br><br>
+    <img :src="getImgUrl('nordwestpassage.jpg')" alt="Nordwest Passage" width="25%">
+    <br>
      <b>Part 4, The Expedition:</b> Back at the ship, the crew is low in morale and arguing to turn back South
  when they get out of the ice pack they are stuck in. Frankenstein encourages them to go on and avoid bravery,
  at least at first. Frankenstein quickly becomes ill and dies with his last words solidified as a famous quote
@@ -81,28 +88,28 @@
       and feel, thou wouldst not desire against me a vengeance greater than that which I feel. Blasted as
       thou wert, my agony was still superior to thine, for the bitter sting of remorse will not cease to rankle
       in my wounds until death shall close them forever. ‘But soon,’ he cried with sad and solemn enthusiasm,
-      ‘I shall die, and what I now feel be no longer felt. Soon these
-       burning miseries will be extinct. I shall ascend my funeral pile triumphantly and exult in the agony of
-      the torturing
-       flames. The light of that conflagration will fade away; my ashes will be swept into the sea by the winds.
-      My spirit will
-       sleep in peace, or if it thinks, it will not surely think thus. Farewell."
+      ‘I shall die, and what I now feel be no longer felt. Soon these burning miseries will be extinct. I shall ascend
+         my funeral pile triumphantly and exult in the agony of the torturing flames. The light of that conflagration
+         will fade away; my ashes will be swept into the sea by the winds. My spirit will sleep in peace, or if it
+         thinks, it will not surely think thus. Farewell."
      </em><br><br>
      The monster then drifts away on the ice to die.
      <br>
      <hr>
      <b>The Pursuit of Knowledge:</b> The first major theme of the work explores the limitations of 'knowledge for
  knowledge's sake'. The paradigmatic belief of the day was the value of disseminating knowledge to the masses for the
- sake of the good. This belief stemmed from enlightenment efforts originating from (at least) Francis Bacon and the
- Protestant Reformation enabling widespread reading of The Bible. Both the shipping captain and Dr. Frankenstein
- are exploring the secrets of nature without explicit justification for their pursuit. They are operating under
- an assumption that Mary Shelley is strongly questioning in this work.
+ sake of the good, stemming from enlightenment efforts originating from (at least) Francis Bacon and the
+ Protestant Reformation. Both the shipping captain and Dr. Frankenstein are exploring the secrets of nature without
+    explicit justification for their pursuit. They are operating under an assumption that Mary Shelley is strongly
+    questioning in this work.
      <br><br>
-     Dr. Frankenstein himself outlines his intellectual inspirations such as Cornelius Agrippa and Albertus Magnus
- who serve as a direct contrast to Frankenstein and the shipping captain's mentality. These clergymen investigated
- the utility of astrology and alchemy in the Catholic tradition as a means to connect and understand God. Frankenstein
+     Dr. Frankenstein himself outlines his intellectual inspirations as Cornelius Agrippa and Albertus Magnus
+ - direct contrasts to Frankenstein and the shipping captain's mentality. These clergymen investigated
+ the utility of astrology and alchemy as a means to connect and understand God. Frankenstein
  works with an important difference in his motivations: he is doing it out of curiosity and, as the reader knows, to
- distract himself of grief. The recklessness proves fateful by the end of the novel.
+ distract himself of grief. The impurity of motivations combined with recklessness prove fatal by the end of the novel.
+    <br>
+    <img :src="getImgUrl('agrippa.jpg')" alt="Agrippa" width="25%">
      <br><br>
      Mary Shelley's point is clear: the cure for recklessness is reflection. Reflection on the question, "Why are we
  doing this?". The monster exists in a state of constant distress to exemplify the importance of this existential
@@ -114,29 +121,30 @@
      "Did I request thee, Maker, from my clay To mould me Man, did I solicit thee From darkness to promote me?"
      </em>
      <br><br>
-     The lament reflects the existential issue of the monster in relation to the lack of contemplation before
- scientific knowledge is pursued. A similar existential issue afflicts the shipping crew when they run into
+     The lament reflects the existential issue of the monster, a monster depicted as wiser and more innocent that the
+    genius Dr. Frankenstein. A similar existential issue afflicts the shipping crew when they run into
  hard conditions near the North Pole. The message, "Without a strong conviction to motivate your actions, nature
  will consume you" resonates strongly in the novel. The associated symbolism is thick when the ship is stuck in
  ice - don't let nature freeze you in place by not knowing what you're doing or else you'll die a cold, lonely death.
      <br><br>
      <b>Agency and Creation:</b> An underlying frustration tracks the relationship between Frankenstein and the
  monster due to constant circling around the question, "Who is at fault for the tragedy of their situation?".
- The monster admits he is lost and scared upon arriving to consciousness so he can be forgiven for not knowing
- how to initially interact or integrate into society. How far can he be forgiven? Is he allowed to murder?
+ The monster admits he is lost and scared upon arriving to consciousness, suggesting he can be forgiven for not knowing
+ how to initially interact or integrate into society. But how far can he be forgiven? Is he allowed to murder?
  Multiple murders? Hold his creator hostage? When does he need to 'get it together'? Is he justified in
- questioning his existence and knowledge of existence a la Job from The Bible doubting God?
+ questioning his existence and knowledge of existence akin to Job doubting God?
      <br><br>
-     And what about Frankenstein? He certaintly created the monster but had honest intentions stemming from his
+     And what about Frankenstein? He certainly created the monster but had honest intentions stemming from his
  'culture'. Should he be hunting it down to 'teach it how to be civil'? Can he be held accountable for the entirely
  unpredictable outcomes of his experiments? If so, what exactly is he supposed to do? The monster is bigger and
  stronger than he is, he didn't want it to run away, he didn't want it to kill anybody... what justification is
  there for his responsibility?
      <br><br>
-     These questions don't have straightforward answers where you can attribute causality unambiguously. In my view,
- the proper attribution of causal agency stems from the foundations of your moral framework and its corresponding
- goals. Do you wish to prevent rogue scientists from experimenting with life? Then Frankenstein is at fault. Do
- you want to justify the violent extermination of monsters? Then the monster is at fault.
+     These questions don't have straightforward answers where you can attribute causality and moral agency
+    unambiguously. In my view, the proper attribution of causal agency stems from the foundations of your moral
+    framework and its corresponding goals. Do you wish to prevent rogue scientists from experimenting with life? Then
+    Frankenstein is at fault. Do you want to justify the violent extermination of monsters? Then the monster is at
+    fault.
      <br><br>
      <em>
        "I, the miserable and the abandoned, am an abortion, to be spurned at, and kicked, and trampled on." - Monster
@@ -149,13 +157,15 @@
  so both are ultimately at fault.
      <br><br>
      <b>Do we start good?:</b> A question puzzling the brightest minds among us, "Does civilization make us good or
- bad?". Who is right - Hobbes or Rousseau? Bartolome de las Casas or Christopher Columbus? Academics or Religions?
+ bad?". Who is right - Hobbes or Rousseau? Bartolome de las Casas or Christopher Columbus? Academics or Clergymen?
  More explicitly, do humans need 'civilization' to be 'civil' or are we most in harmony without technology or
  agriculture?
+    <br>
+    <img :src="getImgUrl('rousseau.jpg')" alt="Rousseau" width="25%">
      <br><br>
      Mary Shelley answers the question through the monster's progression. He starts out quite innocent and ignorant,
  only to be rebuked due to his looks by the community he hoped would validate his identity as a non-monster. The
- corresponding murders and violence come after his peaceful identity was rejected leaving him with only violent
+ corresponding murders and violence come after his peaceful identity was rejected leaving him with only violence
  outrage. As a result, he seeks to abandon civilization to the wilderness, where it is implied the monster belongs.
  The novel leaves the reader with an obligation to connect to the lowest members in society in order to prevent
  righteous indignation.
@@ -166,10 +176,6 @@
 <script>
   export default {
     name: 'Frankenstein',
-    methods: {
-      getImgUrl (pic) {
-        return require('@/assets/blog_posts/books/Frankenstein/' + pic)
-      }
-    }
+    methods: { getImgUrl (pic) { return require('@/assets/blog_posts/books/Frankenstein/' + pic) } }
   }
 </script>
