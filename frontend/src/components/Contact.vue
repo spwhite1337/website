2ed7a2ad50a0a12f2c711d3105ea2b7cca8e7b13
Scott White <spwhite1337@gmail.com>
2020-08-19 17:28:28 -0400

    Own list fo download resume button

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -1,20 +1,24 @@
 <template>
-    <ul>
-        <li>Email (Preferred): spwhite1337@gmail.com</li>
-        <li><button><a @click="showResume">Download Resume (Updated Summer 2020)</a></button></li>
-        <li><a href="https://www.youtube.com/user/spwhite1337" target="_blank">Youtube Channel for SPWhite1337</a></li>
-        <li>Give me money on <a href="https://venmo.com/hubba" target="_blank">Venmo</a> if you like</li>
-        <li><a href="https://twitter.com/ScottDollaBillz" target="_blank">Twitter</a> is my platform of choice</li>
-        <li><a href="https://www.linkedin.com/in/scott-p-white/" target="_blank">LinkedIn</a></li>
-        <li><a href="https://github.com/spwhite1337" target="_blank">GitHub</a></li>
-        <li><a href="https://www.instagram.com/scottdollabills/" target="_blank">
-            Instagram for one selfie a year</a>
-        </li>
-        <li>TikTok so I can rack up the likes:
-            <a href="https://www.tiktok.com/@scottdollabillz" target="_blank">ScottDollaBillz</a>
-        </li>
-        <li>Snapchat for a Dog pic once a month: ScottDollaBillz</li>
-    </ul>
+    <div class="container">
+        <ul>
+            <li><button><a @click="showResume">Download Resume (Updated Summer 2020)</a></button></li>
+        </ul>
+        <ul>
+            <li>Email (Preferred): spwhite1337@gmail.com</li>
+            <li><a href="https://www.youtube.com/user/spwhite1337" target="_blank">Youtube Channel for SPWhite1337</a></li>
+            <li>Give me money on <a href="https://venmo.com/hubba" target="_blank">Venmo</a> if you like</li>
+            <li><a href="https://twitter.com/ScottDollaBillz" target="_blank">Twitter</a> is my platform of choice</li>
+            <li><a href="https://www.linkedin.com/in/scott-p-white/" target="_blank">LinkedIn</a></li>
+            <li><a href="https://github.com/spwhite1337" target="_blank">GitHub</a></li>
+            <li><a href="https://www.instagram.com/scottdollabills/" target="_blank">
+                Instagram for one selfie a year</a>
+            </li>
+            <li>TikTok so I can rack up the likes:
+                <a href="https://www.tiktok.com/@scottdollabillz" target="_blank">ScottDollaBillz</a>
+            </li>
+            <li>Snapchat for a Dog pic once a month: ScottDollaBillz</li>
+        </ul>
+    </div>
 </template>
 <script>
 import Resume from '@/assets/Contact/ScottWhite_Resume.pdf'
