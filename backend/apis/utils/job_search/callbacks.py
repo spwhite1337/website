cc5271a8e37dbdbd2404f1a572542044d3b9c21c
Scott White <spwhite1337@gmail.com>
2020-08-21 19:07:40 -0400

    blank callback

--- backend/apis/utils/job_search/callbacks.py
+++ backend/apis/utils/job_search/callbacks.py
@@ -10,3 +10,7 @@ class DataCallbacks(object):
     def load():
         fn = os.path.join(Config.ROOT_DIR, 'backend', 'apis', 'utils', 'job_search', 'applications.csv')
         return pd.read_csv(fn)
+
+
+class PlotCallbacks(object):
+    pass
