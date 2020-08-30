89a7e8249330ec08908c5d98f4364a22b578cea0
Scott White <spwhite1337@gmail.com>
2020-08-30 11:28:49 -0400

    Add an image to paradise lost && git push -u origin HEAD

--- frontend/src/components/blog_posts/books/ParadiseLost.vue
+++ frontend/src/components/blog_posts/books/ParadiseLost.vue
@@ -3,14 +3,28 @@
 <div>
     <h2>Paradise Lost, by John Milton</h2>
     <hr>
-     Paradise Lost is an epic poem by John Milton that has had reverberating influences on Christianity and Western society since its publication in 1667. Modern conceptions of hell, Satan/Lucifer, and angels can usually be connected directly to Milton's work. It is very difficult to read due to its poetic prose, so I had to read it in parallel with secondary sources that explained the story in plain English (<a href="http://www.paradiselost.org/lmg/Book-1.html" target = "_blank">Example</a>).
+     Paradise Lost is an epic poem by John Milton that has had reverberating influences on Christianity and Western
+    society since its publication in 1667. Modern conceptions of hell, Satan/Lucifer, and angels can usually be
+    connected directly to Milton's work. It is very difficult to read due to its poetic prose, so I had to read
+    it in parallel with secondary sources that explained the story in plain English
+    (<a href="http://www.paradiselost.org/lmg/Book-1.html" target = "_blank">Example</a>).
      <br><br>
-     It is far too massive to tackle the whole thing, so instead I will highlight its relation to conspiratorial thinking - a pattern that is super relevant to navigating the internet and contemporary politics - and comment on the theological implications of Milton's depiction of Adam and Eve.
+     It is far too massive to tackle the whole thing, so instead I will highlight its relation to conspiratorial
+    thinking - a pattern that is super relevant to navigating the internet and contemporary politics - and comment
+    on the theological implications of Milton's depiction of Adam and Eve.
      <br><br>
-     Works like Paradise Lost can really only be understood in a Christian context. If you approach them like "any other work of fiction" you'll lose sight of the insights people draw from the work and its affect on humanity's self-perception ("What does it all mean, man?!?").
+     Works like Paradise Lost can really only be understood in a Christian context. If you approach them like "any
+    other work of fiction" you'll lose sight of the insights people draw from the work and its affect on humanity's
+    self-perception ("What does it all mean, man?!?").
      <hr>
-     <b>Rough Summary:</b> The poem starts out depicting the Holy Spirit inspiring a story-teller to relay the tale of creation in an analogous fashion to greek muses inspiring poems like the Odyssey or the Illiad.
+     <b>Rough Summary:</b> The poem starts out depicting the Holy Spirit inspiring a story-teller to relay the tale
+    of creation in an analogous fashion to greek muses inspiring poems like the Odyssey or the Illiad.
      <br><br>
+    <captioned-image
+          :image="getImgUrl('adam_and_eve.jpg')" alt="Adam and Eve" caption="Adam and Eve"
+          position="right" size="30%">
+    </captioned-image>
+    <br>
      <em>
        "Of Man’s First Disobedience, and the Fruit <br>
         Of that Forbidden Tree, whose mortal taste  <br>
@@ -31,17 +45,33 @@
                  (I.1–26)"
      </em>
      <br><br>
-     From there, Lucifer is chilling in hell with an assortment of biblical demons reconvening after their defeat against the forces of heaven. Frustrated, he plans to undermine God's creation by corrupting it underhandedly rather than attacking it head on. He ventures out of hell by sweet-talking the guardians, Sin and Death, and disguises himself as a Cherub to learn the location of Eden from the angel Uriel and makes his way there disguised as an Eagle (or a generic large bird or something).
+     From there, Lucifer is chilling in hell with an assortment of biblical demons reconvening after their defeat
+    against the forces of heaven. Frustrated, he plans to undermine God's creation by corrupting it underhandedly
+    rather than attacking it head on. He ventures out of hell by sweet-talking the guardians, Sin and Death, and
+    disguises himself as a Cherub to learn the location of Eden from the angel Uriel and makes his way there
+    disguised as an Eagle (or a generic large bird or something).
      <br><br>
-     Satan morphs into a toad and approaches Adam and Eve in the garden to corrupt them. Meanwhile, God is spectating with a watchful eye and sends his angel Gabriel to lay down the hammer on that tricky Satan who bolts after God sends him a vision of golden scales of justice (Satan is the opposite of justice).
+     Satan morphs into a toad and approaches Adam and Eve in the garden to corrupt them. Meanwhile, God is spectating
+    with a watchful eye and sends his angel Gabriel to lay down the hammer on that tricky Satan who bolts after God
+    sends him a vision of golden scales of justice (Satan is the opposite of justice).
      <br><br>
-     Raphael enters the scene and gives Adam and Eve a bit of a pep talk after Satan infiltrated their dreams. Raphael relays warnings about Adam's inquistive nature, reminding him that God is the source of knowledge and there are some concepts beyond his reach. He also reminds Adam that his love for Eve is spiritual in nature, not lustful (even though they still have sex).
+     Raphael enters the scene and gives Adam and Eve a bit of a pep talk after Satan infiltrated their dreams. Raphael
+    relays warnings about Adam's inquisitive nature, reminding him that God is the source of knowledge and there are
+    some concepts beyond his reach. He also reminds Adam that his love for Eve is spiritual in nature, not lustful
+    (even though they still have sex).
      <br><br>
-     About a week after getting scurried away, Satan returns to Paradise and takes the form of a serpent. He corrupts Eve by coercing her to eat from the Tree of Knowledge. She does and frantically tells Adam who follows suit and eats in solidarity with Eve. Then later have sex out of animalistic lust.
+     About a week after getting scurried away, Satan returns to Paradise and takes the form of a serpent. He corrupts
+    Eve by coercing her to eat from the Tree of Knowledge. She does and frantically tells Adam who follows suit and
+    eats in solidarity with Eve. Then later have sex out of animalistic lust.
      <br><br>
-     Watching all along, God sends Jesus to put the situation back in order. Jesus punishes Satan and informs Adam and Eve that they will now experience Pain and Death as a consequence of their sin. Adam throws a fit and argues with Eve throwing blame at her and himself. Ultimately, they resolve to remain obedient to God to spite Satan despite the hardships they will inevitably face.
+     Watching all along, God sends Jesus to put the situation back in order. Jesus punishes Satan and informs Adam and
+    Eve that they will now experience Pain and Death as a consequence of their sin. Adam throws a fit and argues with
+    Eve throwing blame at her and himself. Ultimately, they resolve to remain obedient to God to spite Satan despite
+    the hardships they will inevitably face.
      <br><br>
-     God hears their cries and sends Michael (Archangel) down to help them out. Michael shows Adam the future which is a retelling of the Old Testament full of suffering and death. Michael concludes the poem by showing Adam a vision of Christ's sacrifice which gives him hope to carry on outside of paradise.
+     God hears their cries and sends Michael (Archangel) down to help them out. Michael shows Adam the future which is
+    a retelling of the Old Testament full of suffering and death. Michael concludes the poem by showing Adam a vision
+    of Christ's sacrifice which gives him hope to carry on outside of paradise.
      <br><br>
      <em>
        "They, looking back, all the eastern side beheld <br>
@@ -56,11 +86,18 @@
      </em>
      <br><br>
      <hr>
-     <b>Satan the Conspirator:</b> Focusing on Satan's plans, the poem starts with Satan's realization that he can never beat God and his army head on. He must instead underhandedly corrupt God's creation and win a game with rules he sets himself. Adam and Eve didn't have a chance because they were ultimately a tool for Satan's game.
+     <b>Satan the Conspirator:</b> Focusing on Satan's plans, the poem starts with Satan's realization that he can never
+    beat God and his army head on. He must instead underhandedly corrupt God's creation and win a game with rules he
+    sets himself. Adam and Eve didn't have a chance because they were ultimately a tool for Satan's game.
      <br><br>
-     The accusation John Milton leverages at Satan is the same structure that all prophets leverage against their enemy, "You are evil and motivated by revenge. So you are just going to pursue your goal without any ethical restraints and must be stopped!". More commonly, this takes the form of worshipping idols or false gods - similar to 'playing by rules you're not supposed to play'. In Milton's vision, the evil behavior is a product of his evil nature rather than accidental intentions that spiral off into evilness.
+     The accusation John Milton leverages at Satan is the same structure that all prophets leverage against their enemy,
+    "You are evil and motivated by revenge. So you are just going to pursue your goal without any ethical restraints and
+    must be stopped!". More commonly, this takes the form of worshipping idols or false gods - similar to 'playing by
+    rules you're not supposed to play'. In Milton's vision, the evil behavior is a product of his evil nature rather
+    than accidental intentions that spiral off into evilness.
      <br><br>
-     It is important to note that Milton does not call Satan weak. In fact, Satan is constantly depicted as powerful and mighty:
+     It is important to note that Milton does not call Satan weak. In fact, Satan is constantly depicted as powerful
+    and mighty:
      <br><br>
      <em>
        "Then with expanded wings he stears his flight <br>
@@ -82,15 +119,38 @@
         Not by the sufferance of supernal Power."
      </em>
      <br><br>
-     The explicit depiction of Satan as evil by nature (and powerful) is not exactly accepted by the Catholic Church. There, evil does not exist in itself as all existence is a product of God and therefore good. However, Milton either disagreed with this theology or simplified it for use as a narrative device. The conniving, conspiracizing, underhanded Satan is in direct contrast to an honorable, just, and honest enemies found in other epics. The absolute power of these traits is too much for man alone, but trivial for God and/or Jesus to smite down.
+     The explicit depiction of Satan as evil by nature (and powerful) is not exactly accepted by the Catholic Church.
+    There, evil does not exist in itself as all existence is a product of God and therefore good. However, Milton either
+    disagreed with this theology or simplified it for use as a narrative device. The conniving, conspiracizing,
+    underhanded Satan is in direct contrast to an honorable, just, and honest enemies found in other epics. The absolute
+    power of these traits is too much for man alone, but trivial for God and/or Jesus to smite down.
      <br><br>
-     If you look closely, you'll find this narrative structure all over contemporary society and especially in politics. Hillary Clinton was resentful from her loss to Obama so she cheated to beat Bernie Sanders and tried to undermine the 'rules' of democracy. Donald Trump colluded with Russia to manipulate the public and the Republicans gerrymandered districts to get elected. Those two examples illustrate the profound difficulty we have to decouple 'evil' from 'evil behavior'. In our eyes, an evil person can never do something good and evil behavior isn't enacted by good people. In contrast, John Milton works diligently to decouple these behaviors in his depiction of Adam and Eve. They betray God but not due to their intrinsic evil nature but due to their intrinsic shortcomings.
+     If you look closely, you'll find this narrative structure all over contemporary society and especially in politics.
+    Hillary Clinton was resentful from her loss to Obama so she cheated to beat Bernie Sanders and tried to undermine
+    the 'rules' of democracy. Donald Trump colluded with Russia to manipulate the public and the Republicans
+    gerrymandered districts to get elected. Those two examples illustrate the profound difficulty we have to decouple
+    'evil' from 'evil behavior'. In our eyes, an evil person can never do something good and evil behavior isn't
+    enacted by good people. In contrast, John Milton works diligently to decouple these behaviors in his depiction
+    of Adam and Eve. They betray God but not due to their intrinsic evil nature but due to their intrinsic
+    shortcomings.
      <br><br>
-     <b>Adam and Eve:</b> While the theological implications of Paradise Lost are far too much for me to swallow, the perspective he takes on Adam and Eve is worth a comment. They initially exist as effectively equals - sharing a blissful love in Paradise. Eve commits the first sin, but due to deception by Satan. Adam follows suit, but of his own volition.
+     <b>Adam and Eve:</b> While the theological implications of Paradise Lost are far too much for me to swallow,
+    the perspective he takes on Adam and Eve is worth a comment. They initially exist as effectively equals - sharing
+    a blissful love in Paradise. Eve commits the first sin, but due to deception by Satan. Adam follows suit, but of
+    his own volition.
      <br><br>
-     This belief reflects the controversial portrayal of genders in Christianity. In essence, our differences are irrelevant before God but our roles in society manifest differently. Adam is created first implying his role is to be the 'face' or 'head' of the Garden as a leader in service of God. Eve is created from Adam implying she is in service to both Adam and God. Later on, this relationship is expanded to include Jesus in the hierarchy as well.
+     This belief reflects the controversial portrayal of genders in Christianity. In essence, our differences are
+    irrelevant before God but our roles in society manifest differently. Adam is created first implying his role
+    is to be the 'face' or 'head' of the Garden as a leader in service of God. Eve is created from Adam implying she
+    is in service to both Adam and God. Later on, this relationship is expanded to include Jesus in the hierarchy as
+    well.
      <br><br>
-     To say something like, "Men are above women" or "Adam is above Eve" is too reductionist. They are both nothing without each other but can combine in harmony to properly orient themselves in life. Alone, Eve fell victim to deception by Satan's exploitation of her vanity. Adam's presence could have (i) mitigated her vane concerns by expressing his love to her or (ii) fortified her integrity in the face of temptation. Similarly, with a weakened Eve, Adam loses site of his purpose and perspective. He eats of the tree to regain the solidarity he failed to maintain.
+     To say something like, "Men are above women" or "Adam is above Eve" is too reductionist. They are both nothing
+    without each other but can combine in harmony to properly orient themselves in life. Alone, Eve fell victim to
+    deception by Satan's exploitation of her vanity. Adam's presence could have (i) mitigated her vane concerns by
+    expressing his love to her or (ii) fortified her integrity in the face of temptation. Similarly, with a weakened
+    Eve, Adam loses site of his purpose and perspective. He eats of the tree to regain the solidarity he failed to
+    maintain.
      <br><br>
      <em>
           "Not equal, as their sex not equal seem’d; <br>
@@ -117,7 +177,10 @@
 </template>
 
 <script>
+import CaptionedImage from '@/components/utils/CaptionedImage.vue'
 export default {
-    name: 'ParadiseLost'
+    name: 'ParadiseLost',
+    components: { 'captioned-image': CaptionedImage },
+    methods: { getImgUrl (pic) { return require('@/assets/blog_posts/books/ParadiseLost/' + pic) } }
 }
 </script>
