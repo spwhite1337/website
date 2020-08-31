719e1cd31f478b6951895311c10a3fb29776239f
Scott White <spwhite1337@gmail.com>
2020-08-31 06:23:02 -0400

    WIP brave new world

--- frontend/src/components/blog_posts/books/BraveNewWorld.vue
+++ frontend/src/components/blog_posts/books/BraveNewWorld.vue
@@ -3,17 +3,25 @@
 <div>
     <h2>Brave New World, by Aldous Huxley</h2>
     <hr>
-   <p>
      Rationally optimizing society. An endeavor that only makes sense in the abstract world of politics and theoretical
        societal structures, but fails at the level of a contradictory human. Brave New World is a dystopian novel that
        takes seriously the progressive arguments of techno-futurists in the early 20th century and takes them to their
        horrifying conclusions.
-  </p>
+    <br>
+    <captioned-image
+        :image="getImgUrl('soma.jpg')" alt="Soma"
+        caption="Soma, the most prescient part of the work."
+        position="right" size="30%">
+    </captioned-image>
+    <br>
   </div>
 </template>
 
 <script>
+import CaptionedImage from '@/components/utils/CaptionedImage.vue'
 export default {
-    name: 'BraveNewWorld'
+    name: 'BraveNewWorld',
+    components: { 'captioned-image': CaptionedImage },
+    methods: { getImgUrl (pic) { return require('@/assets/blog_posts/books/BraveNewWorld/' + pic) } }
 }
 </script>
