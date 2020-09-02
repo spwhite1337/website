2a487642ae7b97c0750426a38c614c623b6f5951
Scott White <spwhite1337@gmail.com>
2020-09-01 23:15:22 -0400

    WIP branve new world

--- frontend/src/components/blog_posts/books/BraveNewWorld.vue
+++ frontend/src/components/blog_posts/books/BraveNewWorld.vue
@@ -8,13 +8,21 @@
        societal structures, but fails at the level of a contradictory human. Brave New World is a dystopian novel that
        takes seriously the progressive arguments of techno-futurists in the early 20th century and takes them to their
        horrifying conclusions.
-    <br>
+    <br><br>
+        More interestingly, it is a novelizations of Nietzsche's ideas about <em>The Last Man</em> and his critiques of
+    Liberal Capitalism and Enlightenment reason. Such a topic is beyond the scope of my capabilities but might get
+    revisited in another post someday.
+   <br><hr>
     <captioned-image
         :image="getImgUrl('soma.jpg')" alt="Soma"
         caption="Soma, the most prescient part of the work."
         position="right" size="30%">
     </captioned-image>
-    <br>
+<b>Summary:</b> Set in the 26th Century, Europe had reached it goals of a scientific, progressive, and stable society.
+    The World State cultivates its citizens into different castes that are conditioned to fully in a manner that is
+    mutually beneficially for them and for society as a whole. They find immense joy out of consuming material goods,
+    satiating their sexual appetites, and consuming soma - a psychoactive pharmaceutical that is generously distributed
+    by the World State.
   </div>
 </template>
 
