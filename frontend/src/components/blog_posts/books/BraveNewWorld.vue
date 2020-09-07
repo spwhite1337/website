0fbb4c12041d11a6b403cc160f824766800fe644
Scott White <spwhite1337@gmail.com>
2020-09-07 12:03:09 -0400

    WIP brave new world

--- frontend/src/components/blog_posts/books/BraveNewWorld.vue
+++ frontend/src/components/blog_posts/books/BraveNewWorld.vue
@@ -21,7 +21,7 @@
 <b>Summary:</b> Set in the 26th Century, Europe had reached it goals of a scientific, progressive, and stable society.
     The World State cultivates its citizens into different castes that are conditioned to fully in a manner that is
     mutually beneficially for them and for society as a whole. They find immense joy out of consuming material goods,
-    satiating their sexual appetites, and consuming soma - a psychoactive pharmaceutical that is generously distributed
+    satiating their sexual appetites, and consuming Soma - a psychoactive pharmaceutical that is generously distributed
     by the World State.
     <br><br>
     To enable their profound scientific progress in genetic engineering and psychology, the culture of the World
@@ -32,10 +32,78 @@
     Despite being an alpha, Bernard doesn't fit in well in the World State and suspect he was accidentally subjected to
     alcohol during his birth - resulting in him being a "small" alpha and inspiring insecurities. After causing some
     trouble he ventured to the outskirts of society and meets John, man who grew up outside the hyper-scientific
-    individualism of the World State and instead developed among a psuedo-Native American tribe while learning
+    individualism of the World State and instead developed among a pseudo-Native American tribe while learning
     english from the works of Shakespeare.
     <br><br>
-    Bernard brings John back to the world state where he bonds with the bored and literary-minded Helmholtz. 
+    Bernard brings John back to the world state where he bonds with the bored and literary-minded Helmholtz. He is
+    appalled by the citizenry's aversion to familial bonds, love, God, and their addiction to Soma. Much unrest
+    ensues as the outsider forces the citizens to reflect on the assumptions of their technocratic culture. John is
+    eventually presented to Mond, one of the top ten ranking officials of the World State.
+    <br><br>
+    Mond lifts the curtain of society and outlines the mechanisms only referenced implicitly prior in the novel. The
+    mechanisms of consumption and labor, Soma and sex, conditioning and suppression serve to elevate stability and
+    "rationality" of the World State. The god of the State survives from the sacrifice of citizen's agency and humanity.
+    Importantly, Mond doesn't really outline a "point" to the society other than the avoidance of bad things and
+    stability and order they have glorified. John categorically rejects Mond's philosophy on the grounds it contradicts
+    human nature.
+    <br><br>
+    After the conversation John's countenance deteriorates rapidly. He attempts to exile himself to a lighthouse to
+    adopt a monkish and ascetic lifestyle. To his chagrin, the citizens follow and spectate his strange behavior akin
+    to watching a zoo exhibit. Ultimately, he self-flagellates and hangs himself.
+    <hr>
+    <b>Marx and Liberalism:</b> Huxley brilliantly examines philosophies of politics and society in parallel with
+    more substantive philosophies of humanity and human nature. Bernard's surname is not coincidentally Marx after the
+    famous Viennese as his ideas permeate the World State's culture. The society is designed scientifically and the
+    labor of the lower classes is utilized by the upper classes in accordance with the order of design. Importantly,
+    Marx saw this as intermediate stage towards the final Utopia in contrast to the World State's view of these
+    oppressive labor relations as the end in itself.
+    <br>
+    <captioned-image
+        :image="getImgUrl('marx.jpg')" alt="Marx"
+        caption="Karl Marx, the man who tried to scientifically derive society."
+        position="right" size="30%">
+    </captioned-image>
+    <b>God, The Ubermensch, and The Last Man:</b> The world state citizens are the last men, John and Helmoltz are
+    failed Ubermensch, God is the functional mechanism that destroys the Ubermensch and his function is replaced /
+    improved upon by the scientific progress.
+    <em>
+        "Quite so. I'll read you one of the things he did dream of in a moment. Meanwhile, listen to what this old
+        Arch-Community-Songster said." He opened the book at the place marked by a slip of paper and began to read.
+        "'We are not our own any more than what we possess is our own. We did not make ourselves, we cannot be supreme
+        over ourselves. We are not our own masters. We are God's property. Is it not our happiness thus to view the
+        matter? Is it any happiness or any comfort, to consider that we are our own? It may be thought so by the young
+        and prosperous. These may think it a great thing to have everything, as they suppose, their own way–to depend
+        on no one–to have to think of nothing out of sight, to be without the irksomeness of continual acknowledgment,
+        continual prayer, continual reference of what they do to the will of another. But as time goes on, they, as
+        all men, will find that independence was not made for man–that it is an unnatural state–will do for a while,
+        but will not carry us on safely to the end …'" Mustapha Mond paused, put down the first book and, picking up
+        the other, turned over the pages. "Take this, for example," he said, and in his deep voice once more began
+        to read: "'A man grows old; he feels in himself that radical sense of weakness, of listlessness, of
+        discomfort, which accompanies the advance of age; and, feeling thus, imagines himself merely sick, lulling
+        his fears with the notion that this distressing condition is due to some particular cause, from which, as
+        from an illness, he hopes to recover. Vain imaginings! That sickness is old age; and a horrible disease it
+        is. They say that it is the fear of death and of what comes after death that makes men turn to religion as
+        they advance in years. But my own experience has given me the conviction that, quite apart from any such
+        terrors or imaginings, the religious sentiment tends to develop as we grow older; to develop because, as the
+        passions grow calm, as the fancy and sensibilities are less excited and less excitable, our reason becomes
+        less troubled in its working, less obscured by the images, desires and distractions, in which it used to be
+        absorbed; whereupon God emerges as from behind a cloud; our soul feels, sees, turns towards the source of all
+        light; turns naturally and inevitably; for now that all that gave to the world of sensations its life and
+        charms has begun to leak away from us, now that phenomenal existence is no more bolstered up by impressions
+        from within or from without, we feel the need to lean on something that abides, something that will never
+        play us false–a reality, an absolute and everlasting truth. Yes, we inevitably turn to God; for this
+        religious sentiment is of its nature so pure, so delightful to the soul that experiences it, that it makes
+        up to us for all our other losses.'" Mustapha Mond shut the book and leaned back in his chair. "One of the
+        numerous things in heaven and earth that these philosophers didn't dream about was this" (he waved his hand),
+        "us, the modern world. 'You can only be independent of God while you've got youth and prosperity; independence
+        won't take you safely to the end.' Well, we've now got youth and prosperity right up to the end. What follows?
+        Evidently, that we can be independent of God. 'The religious sentiment will compensate us for all our losses.'
+        But there aren't any losses for us to compensate; religious sentiment is superfluous. And why should we go
+        hunting for a substitute for youthful desires, when youthful desires never fail? A substitute for distractions,
+        when we go on enjoying all the old fooleries to the very last? What need have we of repose when our minds
+        and bodies continue to delight in activity? of consolation, when we have soma? of something immovable,
+        when there is the social order?"
+    </em>
   </div>
 </template>
 
