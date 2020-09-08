9b8f8c7faf9557b69be15d7bb00f5ca648f0c6ac
Scott White <spwhite1337@gmail.com>
2020-09-07 23:51:48 -0400

    Add image for gore annex

--- frontend/src/components/blog_posts/science/PhD.vue
+++ frontend/src/components/blog_posts/science/PhD.vue
@@ -3,11 +3,20 @@
     <h1>Work in Progress: Shield Your Eyes</h1>
     <h2>Label-Free Microfluidic Biosensors with Printed, Floating-Gate Transistors by Scott P. White</h2>
     <hr>
-    <b>Overview:</b> The secret of Ph.D.'s is that the work rarely matters as much as the experience and relationships
-    you make during the process. The challenge of "Becoming an expert in something, no matter how niche" has formative
-    characteristics on the student and instills in them a kind of humility and respect for detail that is hard to find
-    outside the insulated academic environment. Now that I have been in the private sector a few years I understand that
-    my Ph.D. project would have (maybe should have) been discarded after a few weeks of investment.
+    <b>Overview:</b> The secret of doctorate degrees is the work rarely matters as much as the experience and
+    relationships you make during the process. The challenge of "Becoming an expert in something, no matter how niche"
+    has formative characteristics on the student and instills in them a kind of humility and respect for detail that is
+    hard to find outside the insulated academic environment. Now that I have been in the private sector a few years I
+    understand that my Ph.D. project would have (maybe should have) been discarded after a few weeks of investment.
+    <br><br>
+    The people you interact with often give you a fascinating insight into a sub-set of intellectual society. While the
+    members of a Ph.D. organization are generally intelligent, they all very much value intelligence and intellectual
+    pursuits and don't question its adoption as a guiding beacon for living life.
+    <captioned-image
+          :image="getImgUrl('gore_annex.jpg')" alt="Gore Annex"
+          caption="The Chemical Engineering Building at the UMN, 'The Gore Annex'"
+          position="right" size="30%">
+    </captioned-image>
     <br><br>
     <b>Proof of Concept:</b>
     <br><br>
