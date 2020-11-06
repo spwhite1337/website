bf2b7666e70d5f7eeeeeacc7e92aa14dfe22d692
Scott White <spwhite1337@gmail.com>
2020-11-06 17:40:25 -0500

    Comment some lines

--- letsencrypt/cert_renewal.sh
+++ letsencrypt/cert_renewal.sh
@@ -7,8 +7,8 @@ sudo rm -rf ../../docker-volumes
 # Get new certs
 source letsencrypt-prod.sh
 # Shut down and prune cert-container
-sudo docker-compose down
-sudo docker system prune -a
+#sudo docker-compose down
+#sudo docker system prune -a
 # Serve website
-cd ../
-sudo docker-compose -f docker-compose-https.yml up --build -d
+#cd ../
+#sudo docker-compose -f docker-compose-https.yml up --build -d
