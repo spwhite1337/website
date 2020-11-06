91d32cf9d4c47b4f82700037b882480ea5dd6ef4
Scott White <spwhite1337@gmail.com>
2020-11-06 17:45:47 -0500

    Hard-code new certs

--- letsencrypt/cert_renewal.sh
+++ letsencrypt/cert_renewal.sh
@@ -3,7 +3,17 @@ sudo docker system prune -a
 # serve http page
 sudo docker-compose up -d
 # Get new certs
-source letsencrypt-prod.sh
+sudo docker run -i --rm \
+-v /docker-volumes/etc/letsencrypt:/etc/letsencrypt \
+-v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt \
+-v /website/letsencrypt/site:/data/letsencrypt \
+-v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" \
+certbot/certbot \
+certonly --webroot \
+--email spwhite1337@gmail.com --agree-tos --no-eff-email \
+--webroot-path=/data/letsencrypt \
+-d scottpwhite.com -d www.scottpwhite.com
+
 # Shut down and prune cert-container
 #sudo docker-compose down
 #sudo docker system prune -a
