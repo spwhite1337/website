704f7e2f4bcdf6487365e96b2966d17f64920bb9
Ubuntu <ubuntu@ip-172-31-5-57.us-east-2.compute.internal>
2025-11-13 13:04:46 +0000

    update blurb

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -14,17 +14,22 @@
       Midwest tour to Minneapolis, Chicago, and Indianapolis.
       <br><br>
       I am an academy-certified Chemical Engineer with a BS from the University of Iowa and a PhD from The
-      University of Minnesota. Beyond scholarship, I am a private-sector-certified Data Scientist having worked for
-      <a href="https://www.strong.io/" target="_blank">Strong Analytics</a>, a consulting firm in Chicago, and
-      <a href="http://aes.com/" target="_blank">AES</a>, an energy company with a heavy renewables portfolio and my
-      current employer. Of late, my time at AES has been dedicated to improving the product delivered by our utilities businesses.
+      University of Minnesota. Beyond scholarship, I am a private-sector-certified Data Scientist having 
+      worked for <a href="https://www.strong.io/" target="_blank">Strong Analytics</a>, a consulting firm in 
+      Chicago, and <a href="http://aes.com/" target="_blank">AES</a>, an energy company with a heavy 
+      renewables portfolio and my current employer. Of late, my time at AES has been dedicated to improving 
+      the product delivered by our utilities businesses.
       <br>
       <captioned-image
               :image="getImgUrl('indy.jpg')" alt="Indy" caption="Indy White, the Yorkie Terror"
               position="right" size="20%">
       </captioned-image>
       <br>
-      With most of my keyboard-time spent behind a company firewall, I don't have much opportunity to display my work and skills publicly. My best attempts are on disply on my Github and the Data Products section of this website contains (or will contain...) the productionalized version of the best ones. For now, the gambling assisant I am working on is in the prototype stage but I am working to put it in production as fast as I can while maintaining full employment and keeping my newborn daughter alive. 
+      With most of my keyboard-time spent behind a company firewall, I don't have much opportunity to display 
+      my work and skills publicly. My best attempts are on disply on my Github and the Data Products section 
+      of this website contains (or will contain...) the productionalized version of the best ones. For now, 
+      the gambling assisant I am working on is in the prototype stage but I am working to put it in production 
+      as fast as I can while maintaining full employment and keeping my newborn daughter alive. 
       <br><br>
       The Blog is all over the place and not intended to be a professional display - just personal enjoyment. 
       It is best described as a collection of drafted thoughts inspired by things I've read. <em>The Definition of Science</em> is probably my favorite but they are all similar quality.
