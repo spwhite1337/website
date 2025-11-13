35e7fb9e9d2ba2507555269c211b0eccc8042dde
Ubuntu <ubuntu@ip-172-31-5-57.us-east-2.compute.internal>
2025-11-13 12:51:03 +0000

    update blurb

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -24,15 +24,21 @@
               position="right" size="20%">
       </captioned-image>
       <br>
-      Explore the Data Products section to see the gambling assisant I am working on. 
+      With most of my keyboard-time spent behind a company firewall, I don't have much opportunity to display my 
+      work and skills publicly. My best attempts are on disply on my Github and the Data Products section of this
+      website contains (or will contain...) the productionalize version of the best ones. For now, the gambling 
+      assisant I am working on is in the prototype stage but I am working to put it in production as fast as I can while maintaining full employment and keeping my newborn daughter alive. 
       <br><br>
-      The Blog is all over the place and is best described as a collection of drafted thoughts inspired by things I've read. <em>The Definition of Science</em> is probably my favorite but they are all similar quality.
+      The Blog is all over the place and not intended to be a professional display - just personal enjoyment. 
+      It is best described as a collection of drafted thoughts inspired by things I've read. <em>The 
+	      Definition of Science</em> is probably my favorite but they are all similar quality.
       <br><br>
-      Outside of the professional sphere, I am a husband to Brittany, father to Lucy, and a dog-keeper to Indy the Yorkie Terror.
-      Ideologically I'm a tortured combination of Catholic and Liberal with the worst parts of each one
-      contradictorily mixed together. Though the "Catholic" part should be mixed with "American" and integrated
-      beyond an ideology to a Way of Life but I'm too liberal to make any coherent sense of it. I also subscribe to the
-      ideology that doesn't like to put labels on anything but I think that just means I'm a Millennial.
+      Outside of the professional sphere, I am a husband to Brittany, father to Lucy, and a dog-keeper to 
+      Indy the Yorkie Terror. Ideologically I'm a tortured combination of Catholic and Liberal with the 
+      worst parts of each one contradictorily mixed together. Though the "Catholic" part should be mixed 
+      with "American" and integrated beyond an ideology to a Way of Life but I'm too liberal to make any 
+      coherent sense of it. I also subscribe to the ideology that doesn't like to put labels on anything 
+      but I think that just means I'm a Millennial.
     </p>
   </div>
 </template>
