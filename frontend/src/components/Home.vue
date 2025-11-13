3984b4e61857b67d3dc625ef9fa2894fd410ea82
Scott White <spwhite1337@gmail.com>
2025-11-13 13:50:10 +0000

    Update home blurb

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -11,29 +11,36 @@
       </captioned-image>
       <br>
       I'm Scott White and welcome to my site. I hail from the great state of Iowa but have since done a
-      Midwest tour to Minneapolis, Chicago, and Indianapolis.
+      Midwest tour to Minneapolis, Chicago, and Indianapolis. I currently reside in Indianapolis where I am 
+      a proud owner of a plot of land with a house on it.
       <br><br>
       I am an academy-certified Chemical Engineer with a BS from the University of Iowa and a PhD from The
       University of Minnesota. Beyond scholarship, I am a private-sector-certified Data Scientist having 
       worked for <a href="https://www.strong.io/" target="_blank">Strong Analytics</a>, a consulting firm in 
-      Chicago, and <a href="http://aes.com/" target="_blank">AES</a>, an energy company with a heavy 
+      Chicago, and <a href="https://aes.com/" target="_blank">AES</a>, an energy company with a heavy 
       renewables portfolio and my current employer. Of late, my time at AES has been dedicated to improving 
       the product delivered by our utilities businesses.
+      <br><br>
+      With most of my keyboard-time is spent behind a company firewall, I don't have much opportunity to display 
+      my work publicly (n.b., my resume is available in the <a href="Contact">Contact</a>
+      section and has links to publications outlining some work-highlights over the years). 
+      However, technical details and specifics of my personal DS work are on my 
+      <a href="https://github.com/spwhite1337" target="_blank">Github</a> while the 
+      <a href="DataProducts">Data Products</a> section of this website contains 
+      (or will contain...) the productionalized version of the best ones. For now, the gambling assistant I 
+      am working on is in the prototype stage but it will be in production as fast as I can get it there 
+      while maintaining full employment and keeping my newborn daughter alive. 
+      <br><br>
+      The <a href="Blog">Blog</a> is all over the place and not intended to be a professional 
+      display - just personal enjoyment. It is best described as a collection of drafted thoughts inspired 
+      by things I've read. <em>The Definition of Science</em> is probably my favorite but they are all 
+      similar quality.
       <br>
       <captioned-image
               :image="getImgUrl('indy.jpg')" alt="Indy" caption="Indy White, the Yorkie Terror"
               position="right" size="20%">
       </captioned-image>
       <br>
-      With most of my keyboard-time spent behind a company firewall, I don't have much opportunity to display 
-      my work and skills publicly. My best attempts are on my Github while the Data Products section 
-      of this website contains (or will contain...) the productionalized version of the best ones. For now, 
-      the gambling assisant I am working on is in the prototype stage but it will be in production 
-      as fast as I can get it there while maintaining full employment and keeping my newborn daughter alive. 
-      <br><br>
-      The Blog is all over the place and not intended to be a professional display - just personal enjoyment. 
-      It is best described as a collection of drafted thoughts inspired by things I've read. <em>The Definition of Science</em> is probably my favorite but they are all similar quality.
-      <br><br>
       Outside of the professional sphere, I am a husband to Brittany, father to Lucy, and a dog-keeper to 
       Indy the Yorkie Terror. Ideologically I'm a tortured combination of Catholic and Liberal with the 
       worst parts of each one contradictorily mixed together. Though the "Catholic" part should be mixed 
