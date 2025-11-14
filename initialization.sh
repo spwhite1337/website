de81dc469b1aecc46fecda97fc6e86b061173f52
Scott White <spwhite1337@gmail.com>
2025-11-14 04:44:55 +0000

    PEP

--- initialization.sh
+++ initialization.sh
@@ -14,6 +14,7 @@ sudo apt -y install docker-ce
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Download backend data
+# Get aws-cli
 sudo snap install aws-cli --classic
+
 
